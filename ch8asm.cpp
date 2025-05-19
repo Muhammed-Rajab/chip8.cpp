8dc58f8c891acb29ac3845b4c45fd8cce518a325
Rajab Kabeer <muhammedrajab101@gmail.com>
2025-05-20 01:25:41 +0530

    well, i guess i've to finish my ch8asm and ch8dasm works before getting into imgui integration

---
+++ ch8asm.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "include/assembler/assembler.hpp"
+
+int main(int argc, char *argv[]) {
+
+  // Assembler chasm = Assembler::FromFile("./programs/f_you.chasm");
+  // chasm.WriteToFile("./roms/test/f_you.ch8");
+
+  std::cout << "Hello world!" << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ ch8dasm.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "include/disassembler/disassembler.hpp"
+
+int main(int argc, char *argv[]) {
+
+  std::cout << "Hello world!" << std::endl;
+  return EXIT_SUCCESS;
+}
