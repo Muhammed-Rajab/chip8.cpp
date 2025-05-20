eb9686268b2d4f0ffa8aefa1325a7241b509188e
Rajab Kabeer <muhammedrajab101@gmail.com>
2025-05-20 16:44:57 +0530

    shit's clean enough to start working with raylib

--- ch8emu.cpp
+++ ch8emu.cpp
@@ -1,7 +1,3 @@
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_video.h>
 #include <chrono>
 #include <cstdint>
 #include <cstdlib>
@@ -12,7 +8,8 @@
 #include <vector>
 
 #include "./include/chip8.hpp"
-#include "./include/sdl/sdl.hpp"
+
+#include "raylib/raylib.h"
 
 // ====== ROM Loader ======
 std::vector<uint8_t> LoadRomFromFile(const std::string &filename) {
@@ -39,260 +36,39 @@ private:
   // Chip8
   Chip8 &cpu;
 
-  // video
-  int GRID_W{};
-  int GRID_H{};
-
-  int VIDEO_W{};
-  int VIDEO_H{};
-
-  int VIDEO_X_OFF = 0;
-  int VIDEO_Y_OFF = 0;
-
-  // SDL2
-  SDL_Window *window = nullptr;
-  SDL_Renderer *renderer = nullptr;
-
-  float SCREEN_SCALE = 1;
-  int SCREEN_WIDTH = 0;
-  int SCREEN_HEIGHT = 0;
-
   bool quit = false;
 
-  // - font
-  TTF_Font *font = nullptr;
+  void handle_inputs() {}
+
+  void render_video() {}
+
+  void render_ui() {}
+
+  void render() {}
 
 public:
   App(Chip8 &cpu) : cpu(cpu) {
+    // const int screenHeight = 800;
+    const char *title = "CHIP 8";
 
-    // Setup window
-    const int baseWidth = 1280;
-    const int baseHeight = 720;
-    if (SDL::Init()) {
-      window = SDL::CreateWindow("My Window", baseWidth, baseHeight);
-    }
+    // ! RAYLIB SETUP
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_WINDOW_HIGHDPI);
+    InitWindow(800, 800, title);
+    SetTargetFPS(60);
+  }
 
-    // Setup renderer
-    renderer = SDL::CreateRenderer(window);
+  ~App() { CloseWindow(); }
 
-    // get scale
-    SCREEN_SCALE = SDL::GetDisplayScale();
+  void Run() {
+    while (!WindowShouldClose() && !quit) {
+      handle_inputs();
 
-    auto dimensions = SDL::SetupLogicalWidthHeight(renderer, SCREEN_SCALE);
-    SCREEN_WIDTH = dimensions.WIDTH;
-    SCREEN_HEIGHT = dimensions.HEIGHT;
+      render();
 
-    GRID_W = (SCREEN_WIDTH / 2) / 64;
-    GRID_H = GRID_W;
-
-    VIDEO_W = GRID_W * cpu.VIDEO_WIDTH;
-    VIDEO_H = GRID_H * cpu.VIDEO_HEIGHT;
-
-    // ttf setup
-    // TODO: Set it up properly with error handling
-    TTF_Init();
-    font = TTF_OpenFont("./fonts/source-code-pro.ttf", 28);
-    if (!font) {
-      std::cerr << "failed to load font: " << TTF_GetError() << std::endl;
-      exit(EXIT_FAILURE);
+      cpu.Cycle();
     }
   }
-
-  ~App() {
-    TTF_CloseFont(font);
-    TTF_Quit();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-  }
-
-  // handle inputs
-  void handle_inputs() {
-    SDL_Event event;
-
-    while (SDL_PollEvent(&event)) {
-
-      switch (event.type) {
-      case SDL_QUIT:
-        quit = true;
-        break;
-
-      case SDL_KEYDOWN:
-        switch (event.key.keysym.sym) {
-        case SDLK_ESCAPE:
-          quit = true;
-          break;
-        case SDLK_x:
-          cpu.keypad[0x0] = 1;
-          break;
-        case SDLK_1:
-          cpu.keypad[0x1] = 1;
-          break;
-        case SDLK_2:
-          cpu.keypad[0x2] = 1;
-          break;
-        case SDLK_3:
-          cpu.keypad[0x3] = 1;
-          break;
-        case SDLK_q:
-          cpu.keypad[0x4] = 1;
-          break;
-        case SDLK_w:
-          cpu.keypad[0x5] = 1;
-          break;
-        case SDLK_e:
-          cpu.keypad[0x6] = 1;
-          break;
-        case SDLK_a:
-          cpu.keypad[0x7] = 1;
-          break;
-        case SDLK_s:
-          cpu.keypad[0x8] = 1;
-          break;
-        case SDLK_d:
-          cpu.keypad[0x9] = 1;
-          break;
-        case SDLK_z:
-          cpu.keypad[0xA] = 1;
-          break;
-        case SDLK_c:
-          cpu.keypad[0xB] = 1;
-          break;
-        case SDLK_4:
-          cpu.keypad[0xC] = 1;
-          break;
-        case SDLK_r:
-          cpu.keypad[0xD] = 1;
-          break;
-        case SDLK_f:
-          cpu.keypad[0xE] = 1;
-          break;
-        case SDLK_v:
-          cpu.keypad[0xF] = 1;
-          break;
-        }
-        break;
-
-      case SDL_KEYUP:
-        switch (event.key.keysym.sym) {
-        case SDLK_x:
-          cpu.keypad[0x0] = 0;
-          break;
-        case SDLK_1:
-          cpu.keypad[0x1] = 0;
-          break;
-        case SDLK_2:
-          cpu.keypad[0x2] = 0;
-          break;
-        case SDLK_3:
-          cpu.keypad[0x3] = 0;
-          break;
-        case SDLK_q:
-          cpu.keypad[0x4] = 0;
-          break;
-        case SDLK_w:
-          cpu.keypad[0x5] = 0;
-          break;
-        case SDLK_e:
-          cpu.keypad[0x6] = 0;
-          break;
-        case SDLK_a:
-          cpu.keypad[0x7] = 0;
-          break;
-        case SDLK_s:
-          cpu.keypad[0x8] = 0;
-          break;
-        case SDLK_d:
-          cpu.keypad[0x9] = 0;
-          break;
-        case SDLK_z:
-          cpu.keypad[0xA] = 0;
-          break;
-        case SDLK_c:
-          cpu.keypad[0xB] = 0;
-          break;
-        case SDLK_4:
-          cpu.keypad[0xC] = 0;
-          break;
-        case SDLK_r:
-          cpu.keypad[0xD] = 0;
-          break;
-        case SDLK_f:
-          cpu.keypad[0xE] = 0;
-          break;
-        case SDLK_v:
-          cpu.keypad[0xF] = 0;
-          break;
-        }
-        break;
-      }
-    }
-  }
-
-  // render video
-  void render_video(int px, int py) {
-    for (size_t y = 0; y < cpu.VIDEO_HEIGHT; y += 1) {
-      for (size_t x = 0; x < cpu.VIDEO_WIDTH; x += 1) {
-        size_t index = y * cpu.VIDEO_WIDTH + x;
-        uint8_t pixel = cpu.video[index];
-
-        int rx = px + x * GRID_W + VIDEO_X_OFF;
-        int ry = py + y * GRID_H + VIDEO_Y_OFF;
-        SDL_Rect rect{rx, ry, GRID_W, GRID_H};
-
-        if (pixel) {
-          SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-          SDL_RenderFillRect(renderer, &rect);
-          SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-          SDL_RenderDrawRect(renderer, &rect);
-        } else {
-          SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-          SDL_RenderFillRect(renderer, &rect);
-        }
-      }
-    }
-
-    // DRAW A SCREEN BORDER
-    int padding = 5;
-    SDL_Rect border{px + VIDEO_X_OFF - padding, py + VIDEO_Y_OFF - padding,
-                    VIDEO_W + 2 * padding, VIDEO_H + 2 * padding};
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDrawRect(renderer, &border);
-  }
-
-  void render_ui() {
-    SDL_Color color = {255, 255, 255}; // White
-
-    SDL_Surface *surface =
-        TTF_RenderText_Solid(font, "Hello, Emulator!", color);
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-
-    // Set destination rect on screen
-    SDL_Rect dest = {50, 50, surface->w, surface->h};
-
-    // Draw to screen
-    SDL_RenderCopy(renderer, texture, nullptr, &dest);
-
-    // Cleanup
-    SDL_FreeSurface(surface);
-    SDL_DestroyTexture(texture);
-  }
-
-  void render() {
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    render_video((SCREEN_WIDTH - VIDEO_W) / 2, (SCREEN_HEIGHT - VIDEO_H) / 2);
-
-    render_ui();
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderPresent(renderer);
-  }
-
-  bool Quit() const { return quit; }
 };
 
 int main(int argc, char *args[]) {
@@ -305,26 +81,7 @@ int main(int argc, char *args[]) {
 
   App app(cpu);
 
-  while (!app.Quit()) {
-
-    auto frame_start = std::chrono::high_resolution_clock::now();
-
-    app.handle_inputs();
-
-    app.render();
-
-    cpu.Cycle();
-
-    auto frame_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> elapsed = frame_end - frame_start;
-    float frame_time = elapsed.count();
-    float target_time = 1.0f / 60.0f;
-
-    if (frame_time < target_time) {
-      std::this_thread::sleep_for(
-          std::chrono::duration<float>(target_time - frame_time));
-    }
-  }
+  app.Run();
 
   return EXIT_SUCCESS;
 }
